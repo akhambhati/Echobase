7a8f90a63df0c256c6a8a0f819e761b0c9487ce8
Ankit Khambhati <akhambhati@gmail.com>
2016-11-11 15:11:44 -0500

    Added node strength and control centrality

---
+++ Echobase/Network/Metrics/nodetopo.py
@@ -0,0 +1,70 @@
+"""
+Measurements of nodal importance (centrality)
+
+Created by: Ankit Khambhati
+
+Change Log
+----------
+2016/03/10 - Implemented DegrCentral, EvecCentral, SyncCentral pipes
+"""
+
+from __future__ import division
+import numpy as np
+
+from ...Common import errors
+from .globaltopo import synchronizability
+from ..Transforms import lesion
+
+
+def node_strength(adj):
+    """
+    Function for computing average strength of connections from a node
+
+    Parameters
+    ----------
+        adj: ndarray, shape (N, N)
+            Undirected, symmetric adjacency matrix with N nodes
+    """
+
+    # Standard param checks
+    errors.check_type(adj, np.ndarray)
+    errors.check_dims(adj, 2)
+    if not (adj == adj.T).all():
+        raise Exception('Adjacency matrix is not undirected and symmetric.')
+
+    # Get the degree vector of the adj
+    deg_vec = np.sum(adj, axis=0)
+
+    return deg_vec
+
+
+def node_control(adj):
+    """
+    Function for computing control centrality of a node (change in synchronizability)
+
+    Parameters
+    ----------
+        adj: ndarray, shape (N, N)
+            Undirected, symmetric adjacency matrix with N nodes
+    """
+
+    # Standard param checks
+    errors.check_type(adj, np.ndarray)
+    errors.check_dims(adj, 2)
+    if not (adj == adj.T).all():
+        raise Exception('Adjacency matrix is not undirected and symmetric.')
+
+    # Get data attributes
+    n_node = adj.shape[0]
+
+    # Get the original synchronizability
+    base_sync = synchronizability(adj)
+
+    control_vec = np.zeros(n_node)
+    for i in xrange(n_node):
+        adj_lesion = lesion.node_lesion(adj, [i])
+        lesion_sync = synchronizability(adj_lesion)
+
+        control_vec[i] = (lesion_sync-base_sync) / base_sync
+
+    return control_vec
