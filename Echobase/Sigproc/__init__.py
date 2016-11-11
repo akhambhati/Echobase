e7dd020940523ece614c785f77612ab4ffbac9ac
Ankit Khambhati <akhambhati@gmail.com>
2016-11-11 12:18:02 -0500

    Added re-referencing

--- Echobase/Sigproc/__init__.py
+++ Echobase/Sigproc/__init__.py
@@ -1,2 +1,3 @@
 import filters
 import prewhiten
+import reref
---
+++ Echobase/Sigproc/reref.py
@@ -0,0 +1,35 @@
+"""
+Functions for re-referencing the time-varying sensor data.
+
+Created by: Ankit Khambhati
+
+Change Log
+----------
+2016/11/11 - Implemented common_avg_ref
+"""
+
+from __future__ import division
+import numpy as np
+
+from ..Common import errors
+
+
+def common_avg_ref(data):
+    """
+    The common_avg_ref function subtracts the common mode signal from the original
+    signal. Suggested for removing correlated noise, broadly over a sensor array.
+
+    Parameters
+    ----------
+        data: ndarray, shape (T, N)
+            Input signal with T samples over N variates
+    """
+
+    # Standard param checks
+    errors.check_type(data, np.ndarray)
+    errors.check_dims(data, 2)
+
+    # Remove common mode signal
+    data_reref = (data.T - data.mean(axis=1)).T
+
+    return data_reref
