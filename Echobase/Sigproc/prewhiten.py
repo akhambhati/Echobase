909a28846b0418f1c7050db32f4352dd9a4ad353
Ankit Khambhati <akhambhati@gmail.com>
2016-11-11 12:13:25 -0500

    Implemented arone prewhitening

---
+++ Echobase/Sigproc/prewhiten.py
@@ -0,0 +1,45 @@
+"""
+Functions for pre-whitening time-varying data.
+Reduces the autocorrelative structure for a signal -- flattening the power spectrum
+and making temporal structure more Gaussian
+
+Created by: Ankit Khambhati
+
+Change Log
+----------
+2016/11/11 - Implemented ar_one
+"""
+
+from __future__ import division
+import numpy as np
+
+from ..Common import errors
+
+
+def ar_one(data):
+    """
+    The ar_one function fits an AR(1) model to the data and retains the residual as
+    the pre-whitened data
+
+    Parameters
+    ----------
+        data: ndarray, shape (T, N)
+            Input signal with T samples over N variates
+    """
+
+    # Standard param checks
+    errors.check_type(data, np.ndarray)
+    errors.check_dims(data, 2)
+
+    # Retrieve data attributes
+    n_samp, n_chan = data.shape
+
+    # Apply AR(1)
+    data_white = np.zeros((n_samp-1, n_chan))
+    for i in xrange(n_chan):
+        win_x = np.vstack((data[:-1, i],
+                           np.ones(n_samp-1)))
+        w = np.linalg.lstsq(win_x.T, data[1:, i])[0]
+        data_white[:, i] = data[1:, i] - (data[:-1, i]*w[0] + w[1])
+
+    return data_white
