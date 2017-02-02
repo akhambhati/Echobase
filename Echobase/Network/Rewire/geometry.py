a392e4dd7fc6e7b349846df73942cbed3c5bbd4b
Ankit Khambhati <akhambhati@gmail.com>
2017-02-02 15:28:57 -0500

    Added the binned permutation test for rewiring based on categorical connection length

--- Echobase/Network/Rewire/geometry.py
+++ Echobase/Network/Rewire/geometry.py
@@ -83,3 +83,60 @@ def surrogate_trend(adj, dist, mean_ord, std_ord):
     adj_wgt = configuration.convert_conn_vec_to_adj_matr(rank_cfg_vec)
 
     return adj_wgt
+
+
+def bin_perm(adj, dist, n_bins):
+    """
+    Random graphs that preserve the rule that shorter connections are stronger
+
+    Parameters
+    ----------
+        adj: ndarray, shape (N, N)
+            Undirected, symmetric adjacency matrix with N nodes
+
+        dist: ndarray, shape (N, N)
+            Undirected, symmetric distance matrix with N nodes
+
+        n_bins: int
+            Number of bins on distance matrix
+            If n_bins == 1, edge permutation occurs over all distances
+
+    Returns
+    -------
+        adj_wgt: ndarray, shape (N, N)
+            Undirected, symmetric surrogate adjacency matrix with N nodes
+            The edge distribution is preserved, but not strengths
+    """
+
+    # Standard param checks
+    errors.check_type(adj, np.ndarray)
+    errors.check_dims(adj, 2)
+    if not (adj == adj.T).all():
+        raise Exception('Adjacency matrix is not undirected and symmetric.')
+    errors.check_type(dist, np.ndarray)
+    errors.check_dims(dist, 2)
+    if not (dist == dist.T).all():
+        raise Exception('Distance matrix is not undirected and symmetric.')
+    errors.check_type(n_bins, np.int)
+    if not n_bins > 0:
+        raise Exception('Number of bins should be at least 1')
+
+    # Preliminaries
+    n_node = adj.shape[0]
+    adj_wgt = np.zeros((n_node, n_node))
+
+    cfg_vec = configuration.convert_adj_matr_to_cfg_matr(adj.reshape(1, n_node, n_node)).reshape(-1)
+    dist_vec = configuration.convert_adj_matr_to_cfg_matr(dist.reshape(1, n_node, n_node)).reshape(-1)
+    rnd_cfg_vec = np.zeros_like(cfg_vec)
+
+    # Bin the distance vector
+    n_bins += 1
+    bin_edges = np.linspace(dist_vec.min(), dist_vec.max(), n_bins)
+    for bin_ix in xrange(len(bin_edges)-1):
+        edge_ix = np.flatnonzero((dist_vec >= bin_edges[bin_ix]) & (dist_vec < bin_edges[bin_ix+1]))
+        rnd_cfg_vec[edge_ix] = np.random.permutation(cfg_vec[edge_ix])
+
+    # Put into adjacency matrix
+    adj_wgt = configuration.convert_conn_vec_to_adj_matr(rnd_cfg_vec)
+
+    return adj_wgt
