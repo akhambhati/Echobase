c58c0442bea319377907937c49d7d93c1e2c977b
Ankit Khambhati <akhambhati@gmail.com>
2017-04-11 18:35:04 -0400

    Updated with triangular mesh rendering to update surface color directly

--- Echobase/Plotting/render_brain_connectivity.py
+++ Echobase/Plotting/render_brain_connectivity.py
@@ -5,127 +5,143 @@ Created by: Ankit Khambhati
 
 Change Log
 ----------
+2017/04/11 -- Updated the draw function with direct triangular mesh rendering
 2017/02/11 -- Created the draw function
 """
 
 from __future__ import division
 
 import numpy as np
-
 from mayavi import mlab
+import nibabel as nib
 
 
-def draw(vtk_files, node_coords, conn_list=None, brain_rgba=(0.5, 0.5, 0.5, 0.2), node_rgba=(0.30, 0.69, 1.0, 1.0), node_rad=2.5, conn_thr=[0.75, 1.0], conn_cmap='YlOrRd'):
+def draw(surf_vertices, surf_triangles, surf_scalars, surf_cmap, surf_opacity,
+          node_coords, node_sizes, node_colors,
+          conn_list, conn_pct, conn_cmap):
     """
     Draws a brain graph superimposed on the brain surface
 
     Parameters
     ----------
-        vtk_files: list
-            List of paths to VTK surface reconstructions for a brain
+        surf_vertices: numpy.ndarray, shape:(n_vert, 3)
+            Three-dimensional coordinates of each vertex of the surface
 
-        node_coords: numpy.ndarray, shape: (n_node, 3)
-            MNI coordinates for nodes associated with the brain graph
+        surf_triangles: numpy.ndarray, shape:(n_tria, 3)
+            Sets of vertex indices forming a triangle face of the surface
 
-        conn_list: numpy.ndarray, shape: (n_node*(n_node-1)*0.5,)
-            Vector of connection or edge strengths associated with each node
-            Assumes undirected, symmetric connectivity
+        surf_scalars: numpy.ndarray, shape:(n_vert,)
+            Scalars representing magnitude or intensity at each vertex
 
-        brain_rgba: tuple, shape: (4,)
-            RGBA color scheme for the brain surface reconstruction
+        surf_cmap: str
+            Colormap used to map surf_scalars to color
 
-        node_rgba: list of tuples, each list-entry has shape: (4,)
-            Each tuple is the RGBA color scheme for spheres representing each node
-            The size of the list should be equal to n_node
+        surf_opacity: float,  0.0<=x<=1.0
+            Opacity of the colormap
 
-        node_rad: list of floats
-            Radius specifying sphere size associated with each node
-            The size of the list should be equal to n_node
+        node_coords: numpy.ndarray, shape:(n_node, 3)
+            XYZ MNI coordinates of the network nodes
+            Perhaps centroid location of the ROIs
 
-        conn_thr: list, shape: (2,)
-            Lower and upper percentiles for thresholding connections to plot
+        node_sizes: numpy.ndarray, shape:(n_node,)
+            Radii of spheres representing nodes
+
+        node_colors: numpy.ndarray, shape:(n_node, 4)
+            RGBA values to fill spheres representing nodes
+
+        conn_list: numpy.ndarray, shape:(n_node*(n_node-1) / 2,)
+            List of connections between nodes in the network
+            Assumes list represents the upper triangle of the adjacency matrix
+
+        conn_pct: tuple, shape:(2,)
+            Percentile range of connections to plot
 
         conn_cmap: str
-            Colormap to map connection strengths to colors
+            Colormap used to map connection strengths to color
     """
 
     ### Setup the engine and scene
     my_engine = mlab.get_engine()
+    fig = mlab.figure(size=(1000, 1000), bgcolor=(1.0, 1.0, 1.0), engine=my_engine)
 
-    # Import VTK file to pipeline
-    for vtk_file in vtk_files:
-        vtk_source = my_engine.open(vtk_file)
-
-        # Render surface
-        vtk_surface = mlab.pipeline.surface(vtk_source)
-        vtk_surface.actor.property.specular_color = brain_rgba[:3]
-        vtk_surface.actor.property.diffuse_color = brain_rgba[:3]
-        vtk_surface.actor.property.ambient_color = brain_rgba[:3]
-        vtk_surface.actor.property.color = brain_rgba[:3]
-        vtk_surface.actor.property.opacity = brain_rgba[3]
-
-    ### Sensor-Locations
-    # Load Coordinates in MRI-space
+    ### Plot the nodes on the brain
     n_node = node_coords.shape[0]
-    n_conn = np.int(n_node*(n_node-1)*0.5)
+    for n_i in xrange(n_node):
 
-    for ix in xrange(node_coords.shape[0]):
+        node_source = mlab.pipeline.scalar_scatter(node_coords[n_i, 0],
+                                                   node_coords[n_i, 1],
+                                                   node_coords[n_i, 2],
+                                                   figure=fig)
 
-        # Import coordinates into pipeline
-        crd_source = mlab.pipeline.scalar_scatter(node_coords[ix, 0],
-                                                node_coords[ix, 1],
-                                                node_coords[ix, 2])
-        # Render Glyphs for node points
-        crd_surface = mlab.pipeline.glyph(crd_source,
-                                        scale_mode='none',
-                                        scale_factor=node_rad[ix],
-                                        mode='sphere',
-                                        colormap='cool',
-                                        color=node_rgba[ix][:3],
-                                        opacity=node_rgba[ix][3])
+        node_surface = mlab.pipeline.glyph(node_source,
+                                           scale_mode='none',
+                                           scale_factor=node_sizes[n_i],
+                                           mode='sphere',
+                                           color=tuple(node_colors[n_i][:3]),
+                                           opacity=node_colors[n_i][3],
+                                           figure=fig)
 
-    ### Connection-Locations
-    if conn_list is None:
-        print('No connections specified')
-    else:
-        assert len(conn_list) == n_conn
+    ### Plot the connections on the brain
+    n_conn = conn_list.shape[0]
 
-        # Generate all vectors
-        e_start = np.zeros((n_conn, 3))
-        e_vec = np.zeros((n_conn, 3))
+    # Generate connection vectors
+    e_start = np.zeros((n_conn, 3))
+    e_vec = np.zeros((n_conn, 3))
 
-        triu_ix, triu_iy = np.triu_indices(n_node, k=1)
+    triu_ix, triu_iy = np.triu_indices(n_node, k=1)
 
-        for ii, (ix, iy) in enumerate(zip(triu_ix, triu_iy)):
-            e_start[ii, :] = node_coords[ix, :]
-            e_vec[ii, :] = (node_coords[iy, :] - node_coords[ix, :])
+    for ii, (ix, iy) in enumerate(zip(triu_ix, triu_iy)):
+        e_start[ii, :] = node_coords[ix, :]
+        e_vec[ii, :] = (node_coords[iy, :] - node_coords[ix, :])
 
-        # Threshold the connections
-        thresh_lower_ix = np.flatnonzero(conn_list > np.percentile(conn_list, 100*conn_thr[0]))
-        thresh_upper_ix = np.flatnonzero(conn_list < np.percentile(conn_list, 100*conn_thr[1]))
-        thr_ix = np.intersect1d(thresh_lower_ix, thresh_upper_ix)
+    # Threshold the connections
+    lo_thr = np.percentile(conn_list, conn_pct[0])
+    hi_thr = np.percentile(conn_list, conn_pct[1])
+    conn_thr_idx = np.flatnonzero((conn_list >= lo_thr) & (conn_list <= hi_thr))
 
-        # Import vectors (connections) into pipeline
-        edg_source = mlab.pipeline.vector_scatter(e_start[thr_ix, 0],
-                                                e_start[thr_ix, 1],
-                                                e_start[thr_ix, 2],
-                                                e_vec[thr_ix, 0],
-                                                e_vec[thr_ix, 1],
-                                                e_vec[thr_ix, 2])
-        edg_source.mlab_source.dataset.point_data.scalars = conn_list[thr_ix]
+    conn_list = conn_list[conn_thr_idx]
+    conn_list = (conn_list - conn_list.min()) / (conn_list.max() - conn_list.min())
+    e_start = e_start[conn_thr_idx, :]
+    e_vec = e_vec[conn_thr_idx, :]
 
-        edg_surface = mlab.pipeline.vectors(edg_source, colormap=conn_cmap, scale_factor=1,
-                                            line_width=2.0, scale_mode='vector')
+    # Import vectors into pipeline
+    thr_ix = np.argsort(conn_list)
+    conn_source = mlab.pipeline.vector_scatter(e_start[thr_ix, 0],
+                                               e_start[thr_ix, 1],
+                                               e_start[thr_ix, 2],
+                                               e_vec[thr_ix, 0],
+                                               e_vec[thr_ix, 1],
+                                               e_vec[thr_ix, 2], figure=fig)
+    conn_source.mlab_source.dataset.point_data.scalars = conn_list[thr_ix]
 
-        edg_surface.glyph.glyph.clamping = False
-        edg_surface.actor.property.opacity = 0.75
-        edg_surface.module_manager.vector_lut_manager.reverse_lut = False
+    # Change connection attributes
+    conn_surface = mlab.pipeline.vectors(conn_source, colormap=conn_cmap,
+                                         scale_factor=1, line_width=3,
+                                         scale_mode='vector', figure=fig)
+    conn_surface.glyph.glyph.clamping = False
+    conn_surface.actor.property.opacity = 0.75
+    conn_surface.module_manager.vector_lut_manager.reverse_lut = False
 
-        edg_surface.glyph.glyph_source.glyph_source = (
-        edg_surface.glyph.glyph_source.glyph_dict['glyph_source2d'])
-        edg_surface.glyph.glyph_source.glyph_source.glyph_type='dash'
+    conn_surface.glyph.glyph_source.glyph_source = (\
+        conn_surface.glyph.glyph_source.glyph_dict['glyph_source2d'])
+    conn_surface.glyph.glyph_source.glyph_source.glyph_type='dash'
 
-    # Get the associated scene
-    my_scene = my_engine.scenes[0]
-    my_scene.scene.background = (1.0, 1.0, 1.0)
-    my_scene.scene.reset_zoom()
+    ### Plot the colored brain regions
+    surf_source = mlab.pipeline.triangular_mesh_source(surf_vertices[:, 0],
+                                                       surf_vertices[:, 1],
+                                                       surf_vertices[:, 2],
+                                                       surf_triangles,
+                                                       scalars=surf_scalars,
+                                                       opacity=surf_opacity,
+                                                       figure=fig)
+    surf_norms = mlab.pipeline.poly_data_normals(surf_source, figure=fig)
+    surf_norms.filter.splitting = True
+    surf_surf = mlab.pipeline.surface(surf_norms, figure=fig)
+
+    surf_surf.parent.scalar_lut_manager.set(lut_mode=surf_cmap,
+                                             use_default_range=False)
+    lut = surf_surf.module_manager.scalar_lut_manager.lut.table.to_array()
+    lut[:, 3] = surf_opacity
+    surf_surf.module_manager.scalar_lut_manager.lut.table = lut
+
+    return my_engine
