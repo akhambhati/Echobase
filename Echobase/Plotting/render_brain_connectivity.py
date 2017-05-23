d93399bcc862ea5522935af8caf17a5b40a8f6aa
Ankit Khambhati <akhambhati@gmail.com>
2017-05-23 11:55:26 -0400

    Added conditional connection plotting

--- Echobase/Plotting/render_brain_connectivity.py
+++ Echobase/Plotting/render_brain_connectivity.py
@@ -26,7 +26,7 @@ import nibabel as nib
 
 def draw(surf_vertices, surf_triangles, surf_scalars, surf_cmap, surf_opacity,
           node_coords, node_sizes, node_colors,
-          conn_list, conn_pct, conn_cmap):
+          conn_list=None, conn_pct=None, conn_cmap=None):
     """
     Draws a brain graph superimposed on the brain surface
 
@@ -89,46 +89,47 @@ def draw(surf_vertices, surf_triangles, surf_scalars, surf_cmap, surf_opacity,
                                            opacity=node_colors[n_i][3],
                                            figure=fig)
 
-    ### Plot the connections on the brain
-    n_conn = conn_list.shape[0]
+    if conn_list is not None:
+        ### Plot the connections on the brain
+        n_conn = conn_list.shape[0]
 
-    # Generate connection vectors
-    e_start = np.zeros((n_conn, 3))
-    e_vec = np.zeros((n_conn, 3))
+        # Generate connection vectors
+        e_start = np.zeros((n_conn, 3))
+        e_vec = np.zeros((n_conn, 3))
 
-    triu_ix, triu_iy = np.triu_indices(n_node, k=1)
+        triu_ix, triu_iy = np.triu_indices(n_node, k=1)
 
-    for ii, (ix, iy) in enumerate(zip(triu_ix, triu_iy)):
-        e_start[ii, :] = node_coords[ix, :]
-        e_vec[ii, :] = (node_coords[iy, :] - node_coords[ix, :])
+        for ii, (ix, iy) in enumerate(zip(triu_ix, triu_iy)):
+            e_start[ii, :] = node_coords[ix, :]
+            e_vec[ii, :] = (node_coords[iy, :] - node_coords[ix, :])
 
-    # Threshold the connections
-    lo_thr = np.percentile(conn_list, conn_pct[0])
-    hi_thr = np.percentile(conn_list, conn_pct[1])
-    conn_thr_idx = np.flatnonzero((conn_list >= lo_thr) & (conn_list <= hi_thr))
+        # Threshold the connections
+        lo_thr = np.percentile(conn_list, conn_pct[0])
+        hi_thr = np.percentile(conn_list, conn_pct[1])
+        conn_thr_idx = np.flatnonzero((conn_list >= lo_thr) & (conn_list <= hi_thr))
 
-    # Import vectors into pipeline
-    conn_source = mlab.pipeline.vector_scatter(e_start[:, 0],
-                                               e_start[:, 1],
-                                               e_start[:, 2],
-                                               e_vec[:, 0],
-                                               e_vec[:, 1],
-                                               e_vec[:, 2], figure=fig)
-    conn_source.mlab_source.dataset.point_data.scalars = conn_list
+        # Import vectors into pipeline
+        conn_source = mlab.pipeline.vector_scatter(e_start[:, 0],
+                                                   e_start[:, 1],
+                                                   e_start[:, 2],
+                                                   e_vec[:, 0],
+                                                   e_vec[:, 1],
+                                                   e_vec[:, 2], figure=fig)
+        conn_source.mlab_source.dataset.point_data.scalars = conn_list
 
-    #conn_thresh = mlab.pipeline.threshold(conn_source, low=lo_thr, up=hi_thr, figure=fig)
+        #conn_thresh = mlab.pipeline.threshold(conn_source, low=lo_thr, up=hi_thr, figure=fig)
 
-    # Change connection attributes
-    conn_surface = mlab.pipeline.vectors(conn_source, colormap=conn_cmap,
-                                         scale_factor=1, line_width=4, transparent=True,
-                                         scale_mode='vector', figure=fig)
-    conn_surface.glyph.glyph.clamping = False
-    #conn_surface.actor.property.opacity = 0.1
-    conn_surface.module_manager.vector_lut_manager.reverse_lut = False
+        # Change connection attributes
+        conn_surface = mlab.pipeline.vectors(conn_source, colormap=conn_cmap,
+                                             scale_factor=1, line_width=4, transparent=True,
+                                             scale_mode='vector', figure=fig)
+        conn_surface.glyph.glyph.clamping = False
+        #conn_surface.actor.property.opacity = 0.1
+        conn_surface.module_manager.vector_lut_manager.reverse_lut = False
 
-    conn_surface.glyph.glyph_source.glyph_source = (\
-        conn_surface.glyph.glyph_source.glyph_dict['glyph_source2d'])
-    conn_surface.glyph.glyph_source.glyph_source.glyph_type='dash'
+        conn_surface.glyph.glyph_source.glyph_source = (\
+            conn_surface.glyph.glyph_source.glyph_dict['glyph_source2d'])
+        conn_surface.glyph.glyph_source.glyph_source.glyph_type='dash'
 
     ### Plot the colored brain regions
     surf_source = mlab.pipeline.triangular_mesh_source(surf_vertices[:, 0],
