aaadc9710d8d5844063fb27771129d62fa566dbb
Ankit N. Khambhati <akhambhati@gmail.com>
2017-08-29 14:02:06 -0400

    Added conditional stats test dependency to bhp

--- Echobase/Statistics/FDR/fdr.py
+++ Echobase/Statistics/FDR/fdr.py
@@ -5,7 +5,7 @@ from __future__ import division
 import numpy as np
 
 
-def bhp(pv_list, alpha=0.05):
+def bhp(pv_list, alpha=0.05, dependent=False):
     """
     Uses the Benjamini-Hochberg procedure to control the false discovery rate of rejecting the null hypothesis.
 
@@ -16,6 +16,10 @@ def bhp(pv_list, alpha=0.05):
 
     alpha: float
         Family-wise error rate
+
+    dependant: boolean
+        Are the test independent (False) or dependent (True). If dependent then use the
+        Benjamini-Hochberg-Yekutieli procedure
 
     Returns
     -------
@@ -29,9 +33,15 @@ def bhp(pv_list, alpha=0.05):
     n_comp = len(pv_list)
     pv_ix = np.argsort(pv_list)
 
+    if dependent:
+        fac = np.ones(n_comp) / np.arange(1, n_comp+1)
+    else:
+        fac = np.ones(n_comp)
+
+
     sig_ix = []
     for ii, ix in enumerate(pv_ix):
-        if pv_list[ix] <= (alpha * (ii+1) / np.float(n_comp)):
+        if pv_list[ix] <= (alpha * (ii+1) / (fac[ii] * np.float(n_comp)):
             sig_ix.append(ix)
     thresh_ix = np.max(sig_ix)
 
